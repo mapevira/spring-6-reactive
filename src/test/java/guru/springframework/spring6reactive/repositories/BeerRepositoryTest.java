eab3c00e84a931c7070a20c78bc15202224786d9
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-08-20 14:42:22 +0200

    267. Add Create and Update Date with Auditing

--- src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
@@ -1,15 +1,18 @@
 package guru.springframework.spring6reactive.repositories;
 
+import guru.springframework.spring6reactive.config.DatabaseConfig;
 import guru.springframework.spring6reactive.domain.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataR2dbcTest
+@Import(DatabaseConfig.class)
 class BeerRepositoryTest {
 
     @Autowired
