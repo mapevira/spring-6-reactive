0948b68b8225b595669e1a2c38ea3afc28ade4af
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-08-21 09:19:49 +0200

    272. Using Path Variables

--- src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
@@ -5,8 +5,10 @@ import guru.springframework.spring6reactive.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -21,8 +23,14 @@ import reactor.core.publisher.Flux;
 public class BeerController {
 
     public static final String BEER_PATH = "/api/v2/beer";
+    public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
 
     private final BeerService beerService;
+
+    @GetMapping(BEER_PATH_ID)
+    Mono<BeerDTO> getBeerById(@PathVariable("beerId") final Integer beerId) {
+        return beerService.getBeerById(beerId);
+    }
 
     @GetMapping(BEER_PATH)
     Flux<BeerDTO> listBeers() {
--- src/main/java/guru/springframework/spring6reactive/services/BeerService.java
+++ src/main/java/guru/springframework/spring6reactive/services/BeerService.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6reactive.services;
 
 import guru.springframework.spring6reactive.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -14,4 +15,5 @@ public interface BeerService {
 
     Flux<BeerDTO> listBeers();
 
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
