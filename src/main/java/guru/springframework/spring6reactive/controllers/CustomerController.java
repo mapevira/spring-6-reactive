f09ffed4a5c1a0b0ec708289c54f02918f59687d
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-08-22 17:39:02 +0200

    Assignment 32: Assn - Add Customer Exception Tests

--- src/main/java/guru/springframework/spring6reactive/controllers/CustomerController.java
+++ src/main/java/guru/springframework/spring6reactive/controllers/CustomerController.java
@@ -1,82 +1,71 @@
 package guru.springframework.spring6reactive.controllers;
 
-import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.model.CustomerDTO;
-import guru.springframework.spring6reactive.repositories.CustomerRepository;
 import guru.springframework.spring6reactive.services.CustomerService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
  * Created by jt, Spring Framework Guru.
- *
- * @author architecture - raulp
- * @version 21/08/2024 - 17:43
- * @since jdk 1.21
  */
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class CustomerController {
-
     public static final String CUSTOMER_PATH = "/api/v2/customer";
     public static final String CUSTOMER_PATH_ID = CUSTOMER_PATH + "/{customerId}";
 
     private final CustomerService customerService;
-    private final CustomerRepository customerRepository;
-
 
     @DeleteMapping(CUSTOMER_PATH_ID)
-    Mono<ResponseEntity<Void>> deleteById(@PathVariable final Integer customerId) {
-        return customerService.deleteCustomerById(customerId)
-                .then(Mono.fromCallable(() -> ResponseEntity.noContent().build()));
-
+    Mono<ResponseEntity<Void>> deleteById(@PathVariable("customerId") Integer customerId){
+        return customerService.getCustomerById(customerId)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .map(customerDTO -> customerService.deleteCustomerById(customerDTO.getId()))
+                .thenReturn(ResponseEntity
+                        .noContent().build());
     }
 
     @PatchMapping(CUSTOMER_PATH_ID)
-    ResponseEntity<Void> patchExistingCustomer(@PathVariable("customerId") final Integer customerId,
-                                           @Validated @RequestBody final CustomerDTO customerDTO) {
-
-        customerService.patchCustomer(customerId, customerDTO).subscribe();
-
-        return ResponseEntity.noContent().build();
+    Mono<ResponseEntity<Void>> patchExistingCustomer(@PathVariable("customerId") Integer customerId,
+                                                     @Validated @RequestBody CustomerDTO customerDTO){
+        return customerService.patchCustomer(customerId, customerDTO)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .map(updatedDto -> ResponseEntity.ok().build());
     }
 
     @PutMapping(CUSTOMER_PATH_ID)
-    ResponseEntity<Void> updateExistingCustomer(@PathVariable("customerId") final Integer customerId,
-                                            @Validated @RequestBody final CustomerDTO customerDTO) {
-
-        customerService.updateCustomer(customerId, customerDTO).subscribe();
-
-        return ResponseEntity.noContent().build();
+    Mono<ResponseEntity<Void>> updateExistingCustomer(@PathVariable("customerId") Integer customerId,
+                                                      @Validated @RequestBody CustomerDTO customerDTO){
+        return customerService.updateCustomer(customerId, customerDTO)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .map(savedDto -> ResponseEntity.noContent().build());
     }
 
     @PostMapping(CUSTOMER_PATH)
-    Mono<ResponseEntity<Void>> createNewCustomer(@Validated @RequestBody final CustomerDTO customerDTO) {
+    Mono<ResponseEntity<Void>> createNewCustomer(@Validated @RequestBody CustomerDTO customerDTO){
         return customerService.saveNewCustomer(customerDTO)
-                .map(saveDto -> ResponseEntity.created(UriComponentsBuilder
+                .map(savedDto -> ResponseEntity.created(UriComponentsBuilder
                                 .fromHttpUrl("http://localhost:8080/" + CUSTOMER_PATH
-                                        + "/" + saveDto.getId())
+                                        + "/" + savedDto.getId())
                                 .build().toUri())
                         .build());
     }
 
     @GetMapping(CUSTOMER_PATH_ID)
-    Mono<CustomerDTO> getCustomerById(@PathVariable("customerId") final Integer customerId) {
-        return customerService.getCustomerById(customerId);
+    Mono<CustomerDTO> getCustomerById(@PathVariable("customerId") Integer customerId){
+        return customerService.getCustomerById(customerId)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
 
     @GetMapping(CUSTOMER_PATH)
-    Flux<CustomerDTO> listCustomers() {
-        log.info("Listing all customers");
-
+    Flux<CustomerDTO> listCustomers(){
         return customerService.listCustomers();
     }
-
 }
