9bd6329ca35dfbb435df14ac4de2ed1a254c3a50
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-08-22 12:29:06 +0200

    291. Test Delete Not Found

--- src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
@@ -32,8 +32,11 @@ public class BeerController {
 
     @DeleteMapping(BEER_PATH_ID)
     Mono<ResponseEntity<Void>> deleteById(@PathVariable final Integer beerId) {
-        return beerService.deleteBeerById(beerId)
-                .then(Mono.fromCallable(() -> ResponseEntity.noContent().build()));
+        return beerService
+                .getBeerById(beerId)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .map(beerDTO -> beerService.deleteBeerById(beerDTO.getId()))
+                .thenReturn(ResponseEntity.noContent().build());
 
     }
 
