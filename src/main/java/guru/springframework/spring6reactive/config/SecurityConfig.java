2ef8e46188c98dbf5e3a7bfa73268e94e0504588
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-08-27 14:19:45 +0200

    327. Spring Security Configuration

---
+++ src/main/java/guru/springframework/spring6reactive/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring6reactive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ *
+ * @author architecture - raulp
+ * @version 27/08/2024 - 14:11
+ * @since jdk 1.21
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec ->  oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+}
